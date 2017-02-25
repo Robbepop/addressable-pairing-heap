b601237c2b9927ed7df57f576323f7a8eb8bccbe
Robbepop <robbepop@web.de>
2017-02-25 17:14:06 +0100

    added crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
 #![deny(unused_imports)]
-// #![deny(missing_docs)]
+#![deny(missing_docs)]
+
+//! An addressable pairing heap implementation for Rust.
+//! 
+//! Addressable heaps return handles to stored elements that make it possible
+//! to query and edit them. For example this allows for the `decrease_key(h: Handle)` method
+//! that decreases the key (priority) of the element that is associated with the
+//! given handle.
+//! 
+//! This implementation stores elements within a `Stash` that allocates elements
+//! densely within an array.
+//!
+//! It is possible to use custom types as the underlying `Key` type by implementing
+//! the `Key` trait.
 
 extern crate stash;
 extern crate unreachable;
